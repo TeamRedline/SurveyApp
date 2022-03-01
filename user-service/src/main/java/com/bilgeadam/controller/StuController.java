98fdf75b5567b5afba3c9fa1626c602c49224670
gundogduonur <gundogduonur@outlook.com.tr>
2022-03-01 15:52:13 +0300

    jpa

---
+++ user-service/src/main/java/com/bilgeadam/controller/StuController.java
@@ -0,0 +1,5 @@
+package com.bilgeadam.controller;
+
+public class StuController {
+    private String jpaRepositoryFav="asdasd";
+}
---
+++ user-service/src/main/java/com/bilgeadam/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.bilgeadam.controller;
+
+public class StudentController {
+}
