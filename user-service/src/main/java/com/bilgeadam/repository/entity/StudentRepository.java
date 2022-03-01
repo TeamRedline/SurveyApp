00af63c2838cf21af866cfc923e52d6acd8d8a23
gundogduonur <gundogduonur@outlook.com.tr>
2022-03-01 16:25:47 +0300

    login sayfası  eklendi ve repolar oluşturuldu. Iperson reposu id olmadığı için orada tanımlanan repo hata veriyor.sildim o repoyu kendi branchimde.

---
+++ user-service/src/main/java/com/bilgeadam/repository/entity/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.bilgeadam.repository.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
